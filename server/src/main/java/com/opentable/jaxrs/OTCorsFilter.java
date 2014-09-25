d143b08e28e3f81e4b82e2c6f76ee1f4984730bb
Steven Schlansker <sschlansker@opentable.com>
2014-09-25 16:55:08 -0700

    Mark OTCorsFilter as @PreMatching

--- server/src/main/java/com/opentable/jaxrs/OTCorsFilter.java
+++ server/src/main/java/com/opentable/jaxrs/OTCorsFilter.java
@@ -4,10 +4,12 @@ import org.jboss.resteasy.plugins.interceptors.CorsFilter;
 
 import javax.inject.Inject;
 import javax.inject.Singleton;
+import javax.ws.rs.container.PreMatching;
 import javax.ws.rs.ext.Provider;
 
 @Singleton
 @Provider
+@PreMatching
 class OTCorsFilter extends CorsFilter
 {
     @Inject
