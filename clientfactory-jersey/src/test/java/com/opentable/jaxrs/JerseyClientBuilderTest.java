f5cd2f87892369c228ff6d07d029a22a4c228461
Steven Schlansker <sschlansker@opentable.com>
2015-06-26 12:50:03 -0700

    Latest otj-parent

--- clientfactory-jersey/src/test/java/com/opentable/jaxrs/JerseyClientBuilderTest.java
+++ clientfactory-jersey/src/test/java/com/opentable/jaxrs/JerseyClientBuilderTest.java
@@ -14,15 +14,10 @@
 package com.opentable.jaxrs;
 
 import static org.junit.Assert.assertEquals;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
-
-import java.util.concurrent.TimeUnit;
 
 import javax.ws.rs.client.Client;
 
 import org.junit.Test;
-import org.skife.config.TimeSpan;
 
 import com.opentable.config.Config;
 
@@ -56,9 +51,9 @@ public class JerseyClientBuilderTest
     }
 
     private static JaxRsClientConfig makeConfig() {
-        final JaxRsClientConfig conf = mock(JaxRsClientConfig.class);
-        when(conf.socketTimeout()).thenReturn(new TimeSpan(6600, TimeUnit.MILLISECONDS));
-        when(conf.connectTimeout()).thenReturn(new TimeSpan(4400, TimeUnit.MILLISECONDS));
-        return conf;
+        return Config.getFixedConfig(
+                "jaxrs.client.default.socket-timeout", "6600ms",
+                "jaxrs.client.default.connect-timeout", "4400ms")
+            .getBean(JaxRsClientConfig.class);
     }
 }
