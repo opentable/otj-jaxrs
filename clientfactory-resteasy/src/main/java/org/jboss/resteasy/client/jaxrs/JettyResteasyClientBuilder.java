a047dfb950c81515b282d3d17f5758bb97befe89
Adam Richeimer <adamzr@gmail.com>
2018-11-20 11:43:30 -0800

    Support for adding an HTTP proxy to a client on RestEasy

--- clientfactory-resteasy/src/main/java/org/jboss/resteasy/client/jaxrs/JettyResteasyClientBuilder.java
+++ clientfactory-resteasy/src/main/java/org/jboss/resteasy/client/jaxrs/JettyResteasyClientBuilder.java
@@ -3,7 +3,9 @@ package org.jboss.resteasy.client.jaxrs;
 import java.util.Optional;
 import java.util.concurrent.TimeUnit;
 
+import org.apache.commons.lang3.StringUtils;
 import org.eclipse.jetty.client.HttpClient;
+import org.eclipse.jetty.client.HttpProxy;
 import org.eclipse.jetty.util.HttpCookieStore;
 import org.eclipse.jetty.util.ssl.SslContextFactory;
 import org.jboss.resteasy.client.jaxrs.engines.jetty.JettyClientEngine;
@@ -35,6 +37,10 @@ public class JettyResteasyClientBuilder extends ResteasyClientBuilder {
         client.setConnectTimeout(config.getConnectTimeout().toMillis());
         client.setMaxConnectionsPerDestination(config.getHttpClientDefaultMaxPerRoute());
         client.setRemoveIdleDestinations(true);
+        if(StringUtils.isNotBlank(config.getProxyHost()) && config.getProxyPort() != 0) {
+            HttpProxy proxy = new HttpProxy(config.getProxyHost(), config.getProxyPort());
+            client.getProxyConfiguration().getProxies().add(proxy);
+        }
         if (config.isCookieHandlingEnabled()) {
             client.setCookieStore(new HttpCookieStore());
         }
