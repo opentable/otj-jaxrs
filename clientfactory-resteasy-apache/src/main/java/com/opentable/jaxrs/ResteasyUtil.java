75ec16e4e67ed798223bb84913795ebcd97f0cbd
Michael Bell <mbell@opentable.com>
2019-01-29 08:58:58 -0800

    Restore optional resteasy implementation using Apache HttpClient
    Motivation: Support total timeout if people are depending on it.
    Negatives: Current provider structure is weak. User will have to
    
    a) Dep man otj server and exclude otj-jaxrs-resteasy
    b) dep add otj-jaxrs-resteasy-apache
    
    Testing is eh...

---
+++ clientfactory-resteasy-apache/src/main/java/com/opentable/jaxrs/ResteasyUtil.java
@@ -0,0 +1,24 @@
+package com.opentable.jaxrs;
+
+import javax.ws.rs.core.Response;
+
+import org.apache.http.client.methods.HttpRequestBase;
+import org.apache.http.impl.io.ChunkedInputStream;
+import org.jboss.resteasy.client.jaxrs.internal.ClientResponse;
+import org.jboss.resteasy.specimpl.BuiltResponse;
+
+public final class ResteasyUtil {
+    private ResteasyUtil() { BuiltResponse.class.cast(null); } // fool dependency analyzer
+    /**
+     * Given a {@code Response} object, find the associated http request and abort it.
+     * Mostly useful to cleanly close never ending streaming responses -- if you call
+     * {@link ChunkedInputStream#close()} without first aborting it hangs forever.
+     *
+     * @see https://issues.jboss.org/browse/RESTEASY-1478
+     * @see http://mail-archives.apache.org/mod_mbox/hc-httpclient-users/201608.mbox/%3CD93DAC13-ABE5-4E3C-9429-82D3C94838C9%40gmail.com%3E
+     */
+    public static void abortResponse(Response r) {
+        final ClientResponse clientResponse = (ClientResponse)r;
+        ((HttpRequestBase)(clientResponse.getProperties().get(JaxRsClientProperties.ACTUAL_REQUEST))).abort();
+    }
+}
