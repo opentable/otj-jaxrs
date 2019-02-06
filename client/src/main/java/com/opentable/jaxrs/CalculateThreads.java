ff5db4c57dc8cad9f06f01514e54f5595a04158a
Michael Bell <mbell@opentable.com>
2019-02-06 12:49:39 -0800

    tweak

--- client/src/main/java/com/opentable/jaxrs/CalculateThreads.java
+++ client/src/main/java/com/opentable/jaxrs/CalculateThreads.java
@@ -4,6 +4,12 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 // A bit of copy/paste between otj-jaxrs/otj-rest-template - not sure where we could put otherwise other than otj-spring
 // which is a bit.... weird.
+
+/**
+ * Calculate optimal threads for Jetty, etc implementations. Avoids deadlocking
+ * as cores grow.
+ * https://docs.google.com/spreadsheets/d/179upsXNJv_xMWYHZLY2e0456bxoBYbOHW7ORV3m-CxE/edit#gid=288475975
+ */
 public final class CalculateThreads {
     private static final Logger LOG = LoggerFactory.getLogger(CalculateThreads.class);
     private static final int BASE_OPTIMAL_THREADS = 10;
@@ -12,6 +18,14 @@ public final class CalculateThreads {
     private CalculateThreads() {
         /* utility class */
     }
+
+    /**
+     * Calculate optimal threads. If they exceed the specified executorThreads, use optimal threads
+     * instead. Emit appropriate logging
+     * @param executorThreads executor threads - what the caller wishes to use for size
+     * @param name client pool name. Used for logging.
+     * @return int actual threads to use
+     */
     public static int calculateThreads(final int executorThreads, final String name) {
         // For current standard 8 core machines this is 10 regardless.
         // On Java 10, you might get less than 8 core reported, but it will still size as if it's 8
@@ -21,7 +35,7 @@ public final class CalculateThreads {
         if (executorThreads > optimalThreads) {
             // They requested more, sure we can do that!
             threadsChosen = executorThreads;
-            LOG.debug("Requested more than optimal threads. This is not necessarily an error, but you may be overallocating.");
+            LOG.warn("Requested more than optimal threads. This is not necessarily an error, but you may be overallocating.");
         } else {
             // They weren't auto tuning (<0))
             if (executorThreads > 0) {
